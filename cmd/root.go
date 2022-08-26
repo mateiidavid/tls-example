8dbb0159dfd15fbb8797d5be365a480a26364327
Matei David <matei@buoyant.io>
2022-08-26 10:16:45 +0000

    Add comments and clean-up root command
    Signed-off-by: Matei David <matei@buoyant.io>

--- cmd/root.go
+++ cmd/root.go
@@ -23,19 +23,23 @@ var (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "tls-test [sub]",
 	Short: "Test (m)TLS still works despite an expired CA",
-	Long: `A straightforward mTLS example app, meant to test whether Go's HTTPS client and server
+	Long: ` 
+
+    tls-test
+
+    A straightforward mTLS example app, meant to test whether Go's HTTPS client and server
     still go ahead with the mTLS handshake and verification even if a CA in the chain 
-    has expired.`,
-	Run: func(cmd *cobra.Command, args []string) {
-	},
+    has expired.
+
+    `,
 }
 
 var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Start a test HTTPS server",
-	Long: `Start a test HTTPS server with the provided bundle, and leaf certificates.
+	Long: `
+    Start a test HTTPS server with the provided bundle, and leaf certificates.
     The server will return '204' for all HTTP requests made to it.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		srv, err := mkServer(listenAddr, caBundle)
@@ -54,7 +58,8 @@ var serverCmd = &cobra.Command{
 var clientCmd = &cobra.Command{
 	Use:   "client",
 	Short: "Start a test HTTPS client",
-	Long: `Start a test HTTPS client, with the provided bundle, and leaf certificates.
+	Long: `
+    Start a test HTTPS client, with the provided bundle, and leaf certificates.
     The client will send a request to the server, print the status code and exit.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		client, err := mkClient(clientCertPath, clientKeyPath, caBundle)
@@ -94,35 +99,52 @@ func Execute() {
 	}
 }
 
+// Create an HTTP Server that will bind on the provided listen address. A CA
+// certificate path is passed in ('bundlepath') that will be used to read in the
+// CA certs used to authenticate the client.
 func mkServer(listenAddr, bundlePath string) (*http.Server, error) {
+	// Read bundle, add to a set of certificates that will be used to
+	// authenticate the client.
 	certPool, err := configureTrustChain(bundlePath)
 	if err != nil {
 		log.Fatalf("Failed to configure server with trust bundle: %v", err)
 		return nil, err
 	}
 
+	// Create a new HTTP mux, handle only '/'.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		if req.URL.Path != "/" {
 			http.NotFound(w, req)
 			return
 		}
+
+		// Return 204 status code.
 		w.WriteHeader(204)
 	})
 
+	// Create an http server
 	return &http.Server{
 		Addr:    listenAddr,
 		Handler: mux,
 		TLSConfig: &tls.Config{
+			// Min TLS is v1.2
 			MinVersion:               tls.VersionTLS12,
 			PreferServerCipherSuites: true,
 			RootCAs:                  certPool,
-			ClientCAs:                certPool,
+			// Set of CAs that the server will use if it has to authenticate the
+			// client.
+			ClientCAs: certPool,
 		},
 	}, nil
 }
 
+// Create an HTTP client, the function accepts three strings that will instantiate
+// the certificate chain used to authenticate the server, as well as the
+// certificate used by the client.
 func mkClient(certPath, keyPath, bundlePath string) (*http.Client, error) {
+	// Read bundle, add to a set of certificates that will be used to
+	// authenticate the server.
 	certPool, err := configureTrustChain(bundlePath)
 	if err != nil {
 		log.Fatalf("Failed to configure server with trust bundle: %v", err)
@@ -136,29 +158,32 @@ func mkClient(certPath, keyPath, bundlePath string) (*http.Client, error) {
 		return nil, err
 	}
 
-	certPool, err = configureTrustChain(bundlePath)
-	if err != nil {
-		log.Fatalf("Failed to configure server with trust bundle: %v", err)
-	}
-
+	// Create an http client
 	return &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
-				ServerName:   "leaf-server.linkerd.cluster.local",
-				RootCAs:      certPool,
+				// Expected server name is hardcoded to the SAN of the server
+				// leaf certificate
+				ServerName: "leaf-server.linkerd.cluster.local",
+				// RootCAs are the CA certificates the client will use when
+				// authenticating the server.
+				RootCAs: certPool,
+				// Certificates that the client will use.
 				Certificates: []tls.Certificate{clientCert},
 			},
 		},
 	}, nil
 }
 
-func configureTrustChain(bundlePath string) (*x509.CertPool, error) {
-	bundle, err := os.ReadFile(bundlePath)
+// configureTrustChain takes the path to a CA (or a bundle) certificate, and
+// creates a new cert-pool, a set of certificates.
+func configureTrustChain(caPath string) (*x509.CertPool, error) {
+	caCert, err := os.ReadFile(caPath)
 	if err != nil {
 		return nil, err
 	}
 
-	bundlePool := x509.NewCertPool()
-	bundlePool.AppendCertsFromPEM(bundle)
-	return bundlePool, nil
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(caCert)
+	return certPool, nil
 }
